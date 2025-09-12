a059b5cb46e56d974a4707c177d24db0f67d2aa3
manhtrung121292 <manhtrung121292@gmail.com>
2025-09-12 17:03:02 +0200

    Update main.tf

--- lab-tf-102-mod-remote/main.tf
+++ lab-tf-102-mod-remote/main.tf
@@ -31,8 +31,4 @@ module "network" {
 variable "rg_name"  { type = string }
 variable "location" { type = string  default = "westeurope" }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name
-  location = var.location
-  tags     = { env = "lab" }
-}
+
