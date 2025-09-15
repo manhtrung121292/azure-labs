88e118d4d61a17d416843d4da804026d7e38ac2f
manhtrung121292 <manhtrung121292@gmail.com>
2025-09-15 11:21:18 +0200

    Update outputs.tf

--- lab-tf-102-mod-remote/outputs.tf
+++ lab-tf-102-mod-remote/outputs.tf
@@ -1,3 +1,4 @@
 output "rg_name"    { value = azurerm_resource_group.rg.name }
+output "rg_id"  { value = azurerm_resource_group.rg.id }
 output "vnet_id"    { value = module.network.vnet_id }
 output "subnet_ids" { value = module.network.subnet_ids }
